fde7a3ab7ebc615bb5b1f75cee07d4412693f666
Amalric Lombard de Buffières <amalric.debuffieres@gmail.com>
2020-08-02 22:17:06 +0200

    The inputs

---
+++ src/farm/brain/BrainConnector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Amalric Lombard de Buffières on 8/2/20.
+//
+
+#include "BrainConnector.h"
---
+++ src/farm/brain/BrainConnector.h
@@ -0,0 +1,16 @@
+//
+// Created by Amalric Lombard de Buffières on 8/2/20.
+//
+
+#ifndef CREATURES_BRAINCONNECTOR_H
+#define CREATURES_BRAINCONNECTOR_H
+
+
+
+class BrainConnector {
+
+};
+
+
+
+#endif //CREATURES_BRAINCONNECTOR_H
